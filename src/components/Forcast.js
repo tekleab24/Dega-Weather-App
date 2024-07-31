5799b718a8f9b0db6e31e09b2eb94427f9fe59c2
tekleab24 <tekleabmulugeta352@gmail.com>
2024-07-31 15:05:32 +0300

    add some changes

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -4,6 +4,15 @@ function Forcast(props) {
     return (
         <div className='Forcast'>
             <h1>This Week</h1>
+            <img className='weatherIcon'  alt='weather icon' src='icon/1.png'/>
+            <ul>
+            <li>monday</li>   
+            <li>Tuesday</li>   
+            <li>wednesday</li>   
+            <li>thursday</li>   
+            <li>friday</li>   
+
+            </ul>         
         </div>
     );
 }
