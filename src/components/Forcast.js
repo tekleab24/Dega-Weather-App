b641e44b58a58f1d1e6f216d8adaea241fb4e956
tekleab24 <tekleabmulugeta352@gmail.com>
2024-08-06 09:50:44 +0300

    some changes

--- src/components/Forcast.js
+++ src/components/Forcast.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Forcast(props) {
-    return (
-        <div className='Forcast'>
-            <h1>This Week</h1>
-            <img className='weatherIcon'  alt='weather icon' src='icon/1.png'/>
-            <ul>
-            <li>monday</li>   
-            <li>Tuesday</li>   
-            <li>wednesday</li>   
-            <li>thursday</li>   
-            <li>friday</li>   
-
-            </ul>         
-        </div>
-    );
+  return (
+    <div className="Forcast">
+      <h1>This Week</h1>
+      <img className="weatherIcon" alt="weather icon" src="icon/1.png" />
+      <ul>
+        <li>monday</li>
+        <li>Tuesday</li>
+        <li>wednesday</li>
+        <li>thursday</li>
+        <li>friday</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Forcast;
